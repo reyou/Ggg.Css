6282ae7d8b3f13e2da4cbfafd87d5ae63f96946e
Aytekin Ozdemir <aytekin@stripe.com>
2022-02-14 15:00:35 -0600

    new files

--- apps/app-materializecss/js/ggg.material.js
+++
@@ -1,9 +0,0 @@
-var toggleScale = function (sender) {
-    $(sender).toggleClass("scale-out");
-    setTimeout(function () {
-        $(sender).toggleClass("scale-out");
-    }, 2000);
-}
-var flowToggle = function (sender) {
-    $("#flow-p").toggleClass("flow-text");
-}
